b5d3fc8fb050be666af1d7a7119ba30ee9d9aed8
Raj Vardhan Jhanwar <106998407+jhanwarr@users.noreply.github.com>
2022-10-13 18:21:13 -0500

    Update koch-snowflake.rkt

--- Koch_Snowflake/koch-snowflake.rkt
+++ Koch_Snowflake/koch-snowflake.rkt
@@ -3,11 +3,14 @@
 (require "../include/cs151-core.rkt")
 (require "../include/cs151-image.rkt")
 
+; Use (run INTEGER) to test the program, where the INTEGER is the length of
+; one side of the triangle
+
 (: koch-snowflake : Integer -> Image)
 ;Prinitng the koch-snowflake pattern
 (define (koch-snowflake width)
   (cond
-    [(< width 3) (line width 0 "purple")]
+    [(< width 3) (line width 0 "black")]
     [else (beside/align "bottom"
                         (koch-snowflake (cast (exact-floor (/ width 3))
                                               Integer))
@@ -19,8 +22,13 @@
                                               Integer)))]))
 
 
-(koch-snowflake 45)
-(koch-snowflake 90)
-(koch-snowflake 180)
-(koch-snowflake 200)
-(koch-snowflake 360)
+(: run : Integer -> Image)
+; run the world given size of any one side of the triangle
+(define (run length)
+  (above (koch-snowflake length)
+         (beside (rotate 120 (koch-snowflake length))
+                 (rotate 240 (koch-snowflake length)))))
+
+
+;(koch-snowflake 360)
+;(run 200)
